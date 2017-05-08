f8744a742e797dab2e307571ccbbf712fce2f045
孙天民 <1099965906@qq.com>
2017-05-08 17:23:03 +0800

    TransitionListenerAdapter

---
+++ TransitionListenerAdapter.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.photogallery;
+
+import android.support.annotation.NonNull;
+import android.support.transition.Transition;
+
+public class TransitionListenerAdapter implements Transition.TransitionListener {
+	@Override
+	public void onTransitionStart(@NonNull Transition transition) { }
+
+	@Override
+	public void onTransitionEnd(@NonNull Transition transition) { }
+
+	@Override
+	public void onTransitionCancel(@NonNull Transition transition) { }
+
+	@Override
+	public void onTransitionPause(@NonNull Transition transition) { }
+
+	@Override
+	public void onTransitionResume(@NonNull Transition transition) { }
+}
